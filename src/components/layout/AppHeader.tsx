8bcfa94b16a707686476be27747aa128b2186890
aconic-ni <jerco27@gmail.com>
2025-07-25 11:41:19 -0600

    Update AppHeader.tsx

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -2,7 +2,7 @@
 "use client";
 import { Button } from '@/components/ui/button';
 import { useAuth } from '@/context/AuthContext';
-import { FileText, LogOut, UserCircle, Camera } from 'lucide-react';
+import { FileText, LogOut, UserCircle, Camera, FileSpreadsheet } from 'lucide-react';
 import Link from 'next/link';
 
 export function AppHeader() {
@@ -20,15 +20,9 @@ export function AppHeader() {
       <div className="container mx-auto px-4 py-3 max-w-7xl">
         <div className="flex justify-between items-center">
           <div className="flex flex-col items-start">
-            {user?.isStaticUser ? (
-              <div className="flex items-center gap-2 cursor-default">
+            <Link href="/" className="flex items-center gap-2">
                 {renderAppIdentity()}
-              </div>
-            ) : (
-              <Link href="/examiner" className="flex items-center gap-2">
-                {renderAppIdentity()}
-              </Link>
-            )}
+            </Link>
             {user && !loading && (
               <div className="flex w-full items-center justify-center gap-2 text-sm text-muted-foreground mt-1 md:hidden">
                 <UserCircle className="h-5 w-5" />
@@ -46,6 +40,13 @@ export function AppHeader() {
                   <UserCircle className="h-5 w-5" />
                   <span>{user.email}</span>
                 </div>
+                {user.isStaticUser && (
+                   <Button asChild variant="ghost" size="icon" className="text-primary hover:bg-chart-4 hover:text-primary-foreground transition-all duration-300">
+                     <Link href="/reports" aria-label="Ir a la página de reportes">
+                       <FileSpreadsheet className="h-5 w-5" />
+                     </Link>
+                   </Button>
+                )}
                 <Button asChild variant="ghost" size="icon" className="text-primary hover:bg-chart-2 hover:text-primary-foreground transition-all duration-300">
                   <a
                     href="https://aconisani-my.sharepoint.com/:f:/g/personal/asuntos_juridicos_aconic_com_ni/Emrpj4Ss8bhDifpuYc8U_bwBj9r29FGcXxzfxu4PSh2tEQ?e=tkoEC0"
