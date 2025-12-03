986f0cbaaaea328ae02b61ffe6a39c0d1c92a66f
aconic-ni <jerco27@gmail.com>
2025-12-02 21:17:35 -0600

    Update WorksheetDetails.tsx

--- src/components/executive/WorksheetDetails.tsx
+++ src/components/executive/WorksheetDetails.tsx
@@ -1,18 +1,20 @@
-
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
-import { X, Printer, FileText, User, Building, Weight, Truck, MapPin, Anchor, Plane, Globe, Package, ListChecks, FileSymlink, Link as LinkIcon, Eye, Shield, FileBadge, FileKey } from 'lucide-react';
-import type { Worksheet } from '@/types';
-import { Timestamp } from 'firebase/firestore';
+import { X, Printer, FileText, User, Building, Weight, Truck, MapPin, Anchor, Plane, Globe, Package, ListChecks, FileSymlink, Link as LinkIcon, Eye, Shield, FileBadge, FileKey, Edit } from 'lucide-react';
+import type { Worksheet, AppUser } from '@/types';
+import { Timestamp, collection, query, where, getDocs } from 'firebase/firestore';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Badge } from '../ui/badge';
 import { format as formatDateFns } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { aduanas } from '@/lib/formData';
 import { Anexo5Details } from './anexos/Anexo5Details';
+import { cn } from "@/lib/utils";
+import { db } from '@/lib/firebase';
+import Link from 'next/link';
 
 const formatTimestamp = (timestamp: Timestamp | null | undefined): string => {
   if (!timestamp) return 'N/A';
@@ -86,21 +88,18 @@ export const WorksheetDetails: React.FC<{ worksheet: Worksheet; onClose: () => v
       <Card className="mt-6 w-full max-w-5xl mx-auto custom-shadow card-print-styles" id="printable-area">
         <div className="hidden print:block">
             <Image
-                src="/AconicExaminer/imagenes/HEADERSEXA.svg"
+                src="/imagenes/HEADERSEXA.svg"
                 alt="Examen Header"
                 width={800}
                 height={100}
                 className="w-full h-auto mb-4"
                 priority
             />
-             <div className="text-center mb-4">
-                 <CardTitle className="text-xl md:text-2xl font-semibold text-foreground print:text-lg">Hoja de Trabajo: {worksheet.ne}</CardTitle>
-            </div>
         </div>
         <div className="no-print">
             <CardHeader className="print:p-0">
                 <div className="flex justify-between items-start">
-                    <CardTitle className="text-xl md:text-2xl font-semibold text-foreground print:text-lg">Hoja de Trabajo: {worksheet.ne}</CardTitle>
+                    <CardTitle className="text-xl md:text-2xl font-semibold text-foreground print:text-lg no-print">Hoja de Trabajo: {worksheet.ne}</CardTitle>
                     <button onClick={onClose} className="p-1 text-destructive hover:text-destructive/80 no-print">
                         <X className="h-6 w-6" />
                     </button>
@@ -113,18 +112,23 @@ export const WorksheetDetails: React.FC<{ worksheet: Worksheet; onClose: () => v
         <CardContent className="space-y-2 print:space-y-1 print:p-0">
           {/* Main Info */}
           <div className="bg-secondary/30 p-4 rounded-md shadow-sm text-sm print:p-2 print:border print:bg-transparent print:shadow-none space-y-2">
-               <div className="p-2 border-b border-border">
-                  <div className="flex items-center gap-2">
-                    <Building className="mr-1 h-5 w-5 text-primary" />
-                    <p className="text-sm font-medium text-muted-foreground print:text-xs">Consignatario:</p>
-                  </div>
-                  <p className="text-lg text-foreground font-semibold ml-8 print:text-base">{worksheet.consignee}</p>
-                   <div className="text-xs text-muted-foreground ml-8 mt-1">
-                      <span>Creado: {formatTimestamp(worksheet.createdAt)}</span>
-                      {wasModified && (
-                        <span className="ml-2">| Modificado: {formatTimestamp(worksheet.lastUpdatedAt)}</span>
-                      )}
+               <div className="p-2 border-b border-border flex justify-between items-start">
+                  <div>
+                    <div className="flex items-center gap-2">
+                        <Building className="mr-1 h-5 w-5 text-primary" />
+                        <p className="text-sm font-medium text-muted-foreground print:text-xs">Consignatario:</p>
                     </div>
+                    <p className="text-lg text-foreground font-semibold ml-8 print:text-base">{worksheet.consignee}</p>
+                    <div className="text-xs text-muted-foreground ml-8 mt-1">
+                        <span>Creado: {formatTimestamp(worksheet.createdAt)}</span>
+                        {wasModified && (
+                        <span className="ml-2">| Modificado: {formatTimestamp(worksheet.lastUpdatedAt)}</span>
+                        )}
+                    </div>
+                  </div>
+                   <div className="print-only:block">
+                        <Badge className="text-base print:text-sm print:font-bold">Hoja de Trabajo: {worksheet.ne}</Badge>
+                   </div>
               </div>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 print:grid-cols-3 gap-x-6 gap-y-3 print:gap-x-4 print:gap-y-1 ">
                   <DetailItem label="NE" value={worksheet.ne} icon={FileText} />
